52d4a4733934868cadc0b2bf254c2a0889493d3f
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 19:44:57 -0600

    Update Q3.1.sql

--- consultas/Q3.1.sql
+++ consultas/Q3.1.sql
@@ -2,7 +2,7 @@
 WITH ventas_agrupadas AS (
 	SELECT 
 		ct.country AS pais, 
-        YEAR(s.date_sale) AS año_venta,
+        	YEAR(s.date_sale) AS año_venta,
 		MONTH(s.date_sale) AS mes_venta,
 		p.product_name AS producto,
 		SUM(s.quantity) AS unidades_vendidas
@@ -10,10 +10,10 @@ WITH ventas_agrupadas AS (
 	LEFT JOIN products p ON s.id_product = p.id_product
 	LEFT JOIN customers c ON s.id_customer = c.id_customer
 	LEFT JOIN countries ct ON c.id_country = ct.id_country
-    WHERE YEAR(s.date_sale)=2023 OR YEAR(s.date_sale)=2024
+    	WHERE YEAR(s.date_sale)=2023 OR YEAR(s.date_sale)=2024
 	GROUP BY 
 		ct.country, 
-        YEAR(s.date_sale),
+        	YEAR(s.date_sale),
 		MONTH(s.date_sale),
 		p.product_name
 ),
@@ -23,7 +23,7 @@ ventas_ordenadas AS (
 )
 SELECT 
 	pais,
-    año_venta,
+    	año_venta,
 	mes_venta,
 	producto AS producto_mas_vendido,
 	unidades_vendidas
