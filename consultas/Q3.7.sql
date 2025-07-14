1f07244206030e8db0e5c61578973c8c69afda20
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 19:50:01 -0600

    Update Q3.7.sql

--- consultas/Q3.7.sql
+++ consultas/Q3.7.sql
@@ -3,9 +3,9 @@ SELECT
 	ROUND(AVG(cant_productos_comprados),0) AS promedio_compras
 FROM(
 	SELECT
-			c.customer_name AS nombre_cliente,
-			SUM(quantity) AS cant_productos_comprados
+		c.customer_name AS nombre_cliente,
+		SUM(quantity) AS cant_productos_comprados
 	FROM customers c
 	LEFT JOIN sales s ON c.id_customer=s.id_customer
 	GROUP BY c.customer_name
-) AS cliente_compras
+) AS cliente_compras;
