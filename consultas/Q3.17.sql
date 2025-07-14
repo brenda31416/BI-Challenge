2dda4094ddf983824e3da17edf4aa28fd873d1b5
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 21:35:56 -0600

    Update Q3.17.sql

--- consultas/Q3.17.sql
+++ consultas/Q3.17.sql
@@ -3,15 +3,15 @@ SELECT
 	COUNT(*) AS cant_productos_sin_ventas
 FROM(
 	SELECT 
-			CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale)) AS trimestre,
-			product_name AS producto,
-			COUNT(id_sale) AS ventas
-		FROM products p
-		LEFT JOIN sales s ON p.id_product=s.id_product
-		GROUP BY 
-			trimestre,
-			producto
-		HAVING trimestre IN ('Q4-2023','2024') OR trimestre IS NULL
+		CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale)) AS trimestre,
+		product_name AS producto,
+		COUNT(id_sale) AS ventas
+	FROM products p
+	LEFT JOIN sales s ON p.id_product=s.id_product
+	GROUP BY 
+		trimestre,
+		producto
+	HAVING trimestre IN ('Q4-2023','2024') OR trimestre IS NULL
 ) AS producto_ventas
 WHERE ventas=0;
 
@@ -20,14 +20,14 @@ SELECT
 	COUNT(*) AS cant_clientes_sin_compras
 FROM(
 	SELECT 
-			CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale)) AS trimestre,
-			customer_name AS cliente,
-			COUNT(id_sale) AS compras
+		CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale)) AS trimestre,
+		customer_name AS cliente,
+		COUNT(id_sale) AS compras
 		FROM customers c
 		LEFT JOIN sales s ON c.id_customer=s.id_customer
         GROUP BY 
-			trimestre,
-			cliente
+		trimestre,
+		cliente
 		HAVING trimestre IN ('Q4-2023','2024') OR trimestre IS NULL
 ) AS cliente_compras
 WHERE compras=0;
