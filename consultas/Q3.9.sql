d3320f4dce2f3bfd2c3c1f9e300bd405cf4888f5
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 21:31:46 -0600

    Update Q3.9.sql

--- consultas/Q3.9.sql
+++ consultas/Q3.9.sql
@@ -2,12 +2,12 @@
 SELECT
 	CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale)) AS trimestre,
 	country AS pais,
-    COUNT(id_sale) AS total_ventas
-    FROM countries ct
-    LEFT JOIN customers c ON ct.id_country=c.id_country
-    LEFT JOIN sales s ON c.id_customer=s.id_customer
-    GROUP BY 
-		ct.country, 
+    	COUNT(id_sale) AS total_ventas
+FROM countries ct
+LEFT JOIN customers c ON ct.id_country=c.id_country
+LEFT JOIN sales s ON c.id_customer=s.id_customer 
+GROUP BY 
+	ct.country, 
         CONCAT('Q',QUARTER(date_sale),'-',YEAR(date_sale))
-	HAVING trimestre='Q3-2023' OR trimestre='Q1-2024'
-    ORDER BY trimestre, pais;
+HAVING trimestre='Q3-2023' OR trimestre='Q1-2024'
+ORDER BY trimestre, pais;
