ae47031020ebd2b6aa471a743e593caacffe71cc
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 21:30:56 -0600

    Update Q3.8.sql

--- consultas/Q3.8.sql
+++ consultas/Q3.8.sql
@@ -1,6 +1,6 @@
 #¿Cuál es la distribución de clientes por frecuencia de compra? 
 WITH cliente_compras AS(	
-    SELECT
+	SELECT
 		customer_name AS cliente,
 		COUNT(id_sale) AS compras
 		FROM customers c
@@ -10,13 +10,13 @@ WITH cliente_compras AS(
 SELECT
 	CASE
 		WHEN (compras>=0 AND compras<=5) THEN '0-5'
-        WHEN (compras>5 AND compras<=10) THEN '6-10'
-        WHEN (compras>10 AND compras<=15) THEN '11-15'
-        WHEN (compras>15 AND compras<=20) THEN '16-20'
-        ELSE '+20'
+        	WHEN (compras>5 AND compras<=10) THEN '6-10'
+        	WHEN (compras>10 AND compras<=15) THEN '11-15'
+        	WHEN (compras>15 AND compras<=20) THEN '16-20'
+        	ELSE '+20'
 	END AS frecuencia_compra,
-    COUNT(cliente) AS total_clientes
-    FROM cliente_compras
-    GROUP BY frecuencia_compra;
+    	COUNT(cliente) AS total_clientes
+FROM cliente_compras
+GROUP BY frecuencia_compra;
     
 	
