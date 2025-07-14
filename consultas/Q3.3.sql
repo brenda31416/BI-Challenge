348fd2969ff84d5f3b3b1cbbdadf5c196973f744
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2025-07-13 19:47:08 -0600

    Update Q3.3.sql

--- consultas/Q3.3.sql
+++ consultas/Q3.3.sql
@@ -11,8 +11,8 @@ WITH producto_ventastotal AS(
 producto_ventastatus AS (
 	SELECT
 		product_name AS producto,
-        COUNT(CASE WHEN os.description = 'Returned' THEN 1 END) AS ventas_devueltas,
-        COUNT(CASE WHEN os.description = 'Cancelled' THEN 1 END) AS ventas_canceladas 
+        	COUNT(CASE WHEN os.description = 'Returned' THEN 1 END) AS ventas_devueltas,
+        	COUNT(CASE WHEN os.description = 'Cancelled' THEN 1 END) AS ventas_canceladas 
 	FROM products p 
 	LEFT JOIN sales s ON p.id_product=s.id_product
 	LEFT JOIN order_status os ON s.id_status=os.id_status
@@ -21,7 +21,7 @@ producto_ventastatus AS (
 SELECT 
 	pvt.producto,
 	CASE WHEN pvt.total_ventas!=0 THEN CONCAT(ROUND(pvs.ventas_devueltas*100/pvt.total_ventas,0),'%') ELSE 0 END AS tasa_devolucion,
-    CASE WHEN pvt.total_ventas!=0 THEN CONCAT(ROUND(pvs.ventas_canceladas*100/pvt.total_ventas,0),'%') ELSE 0 END AS tasa_cancelacion
+    	CASE WHEN pvt.total_ventas!=0 THEN CONCAT(ROUND(pvs.ventas_canceladas*100/pvt.total_ventas,0),'%') ELSE 0 END AS tasa_cancelacion
 FROM producto_ventastotal pvt  
 LEFT JOIN producto_ventastatus pvs ON pvt.producto=pvs.producto
 GROUP BY pvt.producto
